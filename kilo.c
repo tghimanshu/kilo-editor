db8316cd8cd9147fd2352a00447ba0efe110fd15
Himanshu Gohil <himanshugohil234@gmail.com>
2026-02-04 04:38:33 +0000

    Removed Documentation Commends #RTFM

--- kilo.c
+++ kilo.c
@@ -45,8 +45,6 @@ struct editorConfig {
 
 struct editorConfig E;
 
-// struct termios orig_termios;
-
 /***** TERMINAL *****/
 
 void die(const char *s) {
@@ -68,49 +66,9 @@ void enableRawMode() {
 
   struct termios raw = E.orig_termios; // this copies the original state
 
-  // disable software flow control
-  // IXON: disable Ctrl-S and ctrl-Q from being interpreted
-  // -- Ctrl-S otherwise stops all output to the terminal
-  // -- Ctrl-Q otherwise resumes output to the terminal
-  // ICRNL: disable carriage return (CR) to newline (NL) translations
-  // -- This prevents Enter key from being translated to '\n'
-  // -- Without this, pressing Enter would send '\r' which is carriage return
-  // -- fixes Ctrl-M behavior in short (Ctrl-M is carriage return character)
-  // BRKINT: disable break condition from sending SIGINT signal
-  // -- This prevents Ctrl-\ from terminating the program
-  // INPCK: enables parity checking
-  // -- Not commonly used, but we disable it for completeness
-  // ISTRIP: disable stripping of the 8th bit
-  // -- This allows us to read 8-bit characters
-  //
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-
-  // OPOST: disables all output processing
-  // -- This prevents newline characters from being translated to carriage
-  // Return
-  // -- newline on some systems
-  //
   raw.c_oflag &= ~(OPOST);
-
-  // CS8: set character size to 8 bits per byte
-  // -- it's a bit mask that sets the character size to 8 bits per byte
   raw.c_cflag |= (CS8); // set character size to 8 bits per byte
-
-  // disable canonical mode and echoing
-  // ECHO: input characters are echoed back to the terminal and is dsiabled here
-  // ICANON: canonical mode is disabled here, which means we are now in raw mode
-  // -- (non-canonical mode)
-  // -- This will allow us to read input byte-by-byte instead of line-by-line
-  // ISIG: disable signal characters (like Ctrl-C and Ctrl-Z) from being
-  // interpreted
-  // -- Ctrl-C otherwise sends SIGINT to the program, terminating it
-  // -- Ctrl-Z otherwise sends SIGTSTP to the program, suspending it to the
-  // -- background
-  // IEXTEN: Disable implementation-defined input processing
-  // -- on some systems, this disables Ctrl-V from being interpreted
-  // -- Ctrl-V otherwise allows the next character to be inserted
-  // -- literally, even if it's a special character like Ctrl-C
-  //
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   raw.c_cc[VMIN] =
@@ -166,19 +124,9 @@ int getCursorPosition(int *rows, int *cols) {
 int getWindowSize(int *rows, int *cols) {
   // Cursor Positions: https://vt100.net/docs/vt100-ug/chapter3.html#CUF
 
-  // ioctl: input/output control
-  // STDOUT_FILENO: file descriptor for standard output
-  // TIOCGWINSZ: terminal input/output control code to get window size
-  // ws: pointer to struct winsize to store the window size
-  // returns -1 on failure, 0 on success
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    // if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    // move cursor to bottom-right corner
-    // \x1b is the escape character in hexadecimal
-    // [999C moves the cursor 999 columns to the right
-    // [999B moves the cursor 999 rows down
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
       return -1;
 
@@ -243,69 +191,24 @@ void editorDrawRows(struct abuf *ab) {
       abAppend(ab, "~", 1);
     }
 
-    // remove warning for truncated literals
-    // int len = y > 99 ? 99 : y;
-
-    // if (y == 0) {
-    //   snprintf(line, 8, " %d  \r\n", len + 1);
-    // } else if (y < 9) {
-    //   snprintf(line, 8, " %d ~\r\n", len + 1);
-    // } else {
-    //   snprintf(line, 8, "%d ~\r\n", len + 1);
-    // }
-
-    // clear line
-    // \x1b is the escape character in hexadecimal
-    // [K is the ANSI escape code to clear from cursor to the end of the line
-    // default is [0K whic clears to the right of the cursor
-    // we use [K which is equivalent to [0K
-    // [1K would clear form the beginning of the line to the cursor
-    // [2K would clear the entire line
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
-
-    // write(STDOUT_FILENO, line, 6);
   }
-  // int y;
-  // for (y = 0; y < E.screenrows; ++y) {
-  //   write(STDOUT_FILENO, "~", 1);
-  //
-  //   if (y < E.screenrows - 1) {
-  //     write(STDOUT_FILENO, "\r\n", 2);
-  //   }
-  // }
 }
 
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
 
-  // hide cursor
-  // \x1b is the escape character in hexadecimal
-  // [?25l is the ANSI escape code to hide the cursor
   abAppend(&ab, "\x1b[?25l", 6);
-  // clear screen
-  // \x1b is the escape character in hexadecimal
-  // [2J is the ANSI escape code to clear the entire screen
-  // 4 is the length of the escape sequence
-  // write(STDOUT_FILENO, "\x1b[2J", 4); // clear entire screen
-  // abAppend(&ab, "\x1b[2J", 4);
-  //
-  // reposition cursor to top-left corner
-  // \x1b is the escape character in hexadecimal
-  // [H is the ANSI escape code to move the cursor to the home position
-  // top-left
-  // write(STDOUT_FILENO, "\x1b[H", 3); // reposition cursor to top-left corner
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
   abAppend(&ab, "\x1b[?25h", 6); // show cursor
-
-  // write(STDOUT_FILENO, "\x1b[H", 3);
-  // write(STDOUT_FILENO, "\x1b[3C", 4);
+                                 //
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
 }
