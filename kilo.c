968998a4d7ea80e785ccf5ad4ffa5980371bd332
Himanshu Gohil <himanshugohil234@gmail.com>
2026-03-17 22:58:46 +0530

    Feat: Saving File

--- kilo.c
+++ kilo.c
@@ -8,6 +8,7 @@
 
 // #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -301,6 +302,25 @@ void editorInsertChar(int c) {
 
 /***** File I/O *****/
 
+char *editorRowsToString(int *buflen) {
+  int totlen = 0;
+  int j = 0;
+  for (j = 0; j < E.numrows; ++j)
+    totlen += E.row[j].size + 1;
+  *buflen = totlen;
+
+  char *buf = malloc(totlen);
+  char *p = buf;
+  for (j = 0; j < E.numrows; ++j) {
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p += '\n';
+    p++;
+  }
+
+  return buf;
+}
+
 void editorOpen(char *filename) {
   free(E.filename);
   E.filename = strdup(filename);
@@ -325,6 +345,20 @@ void editorOpen(char *filename) {
 
   free(line);
   fclose(fp);
+}
+
+void editorSave() {
+  if (E.filename == NULL)
+    return;
+
+  int len;
+  char *buf = editorRowsToString(&len);
+
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  ftruncate(fd, len);
+  write(fd, buf, len);
+  close(fd);
+  free(buf);
 }
 
 /***** APPEND BUFFER *****/
@@ -571,6 +605,10 @@ void editorProcessKeypress() {
     write(STDOUT_FILENO, "\x1b[2J", 4); // clear entire screen
     write(STDOUT_FILENO, "\x1b[H", 3);  // cursor to top-left corner
     exit(0);
+    break;
+
+  case CTRL_KEY('s'):
+    editorSave();
     break;
 
   case BACKSPACE:
